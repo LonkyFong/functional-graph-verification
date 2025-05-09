493f6fbc9b4c9f4eccd4d6c322783fb6ee5bb779
leopold <frieberger.leopold@outlook.com>
2025-05-09 17:18:42 +0200

    current equational specifications of IG basic moved to RG (without proofs)

--- project/inductive/basic/inductive_graph_basic_theory.v
+++ project/inductive/basic/inductive_graph_basic_theory.v
@@ -14,14 +14,22 @@ Require Import Coq.Sets.Ensembles.
 
 (* Defining Conversion from basic Inductive Graph to Relational Graph *)
 
+(* This works better in proofs than the library Add for ensembles *)
+Definition _customEnsembleAdd {A : Type} (a : A) (en : Ensemble A) : Ensemble A :=
+  fun (x : A) => x = a \/ en x
+.
+
+Definition _listToEnsemble {A : Type} (az : list A) : Ensemble A :=
+  fold_right _customEnsembleAdd (Empty_set A) az
+.
+
 
 (* Adds a node and its in- and out- going edges (= its IG context) to an RG.
     Assumes that the neighboring nodes exist *)
 Definition _extendByContext (node : Node) (froms tos : NatSet.t) (rg : RG nat) : RG nat.
 Proof.
     refine {|
-        gr_nodes := fun (n : nat) => n = node \/ NatSet.In n froms \/ NatSet.In n tos \/ rg.(gr_nodes) n;
-
+        gr_nodes := fun (n : nat) => NatSet.In n froms \/ NatSet.In n tos \/ (_customEnsembleAdd node rg.(gr_nodes))  n;
         gr_edges := fun (n0 n1 : nat) =>
                                 (NatSet.In n0 froms /\ n1 = node)
                                 \/ (n0 = node /\ NatSet.In n1 tos)
@@ -32,6 +40,8 @@ Proof.
     |}.
     unfold valid_cond. pose proof rg.(gr_valid). firstorder.
 Defined.
+
+
 
 
 Definition _accTo_RG (node : Node) (rgIg : RG nat * IG) : RG nat * IG :=
@@ -106,7 +116,7 @@ Definition RG_isEmpty (rg : RG nat) : Prop :=
 Definition _eliminate (node : Node) (rg : RG nat) : RG nat.
 Proof.
   refine {|
-      gr_nodes :=  Subtract nat (rg.(gr_nodes)) node;
+      gr_nodes := Subtract nat (rg.(gr_nodes)) node;
       gr_edges := fun (n0 n1 : nat) => n0 <> node /\ n1 <> node /\  rg.(gr_edges) n0 n1;
       gr_valid := _
   |}.
@@ -154,7 +164,7 @@ Proof.
   unfold valid_cond. pose proof rg.(gr_valid). firstorder.
   - congruence.
   - congruence.
-Qed.
+Defined.
 
 
 Definition _insEdges (edges : list (Node * Node)) (rg : RG nat) : RG nat :=
@@ -165,3 +175,97 @@ Definition RG_mkGraph (nodes : list Node) (edges : list (Node * Node)) : RG nat 
  
 Definition RG_labNodes (rg : RG nat) : Ensemble Node :=
   rg.(gr_nodes).
+
+
+(* Here come some proofs: *)
+
+(* Here start "meaningful statements" *)
+
+(* 5 statements on inserting (helpers for mkGraph): update, insEdge, insEdges, insNode, insNodes *)
+
+Lemma RG_insEdge_does_not_add_node : forall (edge : (Node * Node)) (rg : RG nat),
+  RG_labNodes (_insEdge edge rg) = RG_labNodes rg.
+Proof.
+Admitted.
+
+
+Lemma RG_insEdges_does_not_add_nodes : forall (edges : list (Node * Node)) (rg : RG nat) (x : Node),
+  RG_labNodes (_insEdges edges rg) = RG_labNodes rg.
+Proof.
+Admitted.
+
+
+Lemma RG_insNode_any_ins_node : forall (node : Node) (rg : RG nat),
+  RG_labNodes (_insNode node rg) = _customEnsembleAdd node (RG_labNodes rg).
+Proof.
+Admitted.
+
+
+
+
+Lemma RG_insNodes_any_ins_all_nodes : forall (nodes : list Node) (rg : RG nat),
+  RG_labNodes (_insNodes nodes rg) = Union nat (_listToEnsemble nodes) (RG_labNodes rg).
+Proof.
+Admitted.
+
+
+
+Lemma RG_insEdge_on_empty_is_empty : forall (edge : Node * Node),
+  _insEdge edge RG_empty = RG_empty.
+Proof.
+Admitted.
+
+
+Lemma RG_insEdges_on_empty_is_empty : forall (edges : list (Node * Node)),
+  _insEdges edges RG_empty = RG_empty.
+(* This proof is very similar to "insEdges_does_not_add_nodes", but using it here it is more complicated than just doing it again  *)
+Proof.
+Admitted.
+
+
+
+(* "big" statement: *)
+Theorem RG_mkGraph_any_ins_all_nodes : forall (nodes : list Node) (edges : list (Node * Node)),
+  RG_labNodes (RG_mkGraph nodes edges) = _listToEnsemble nodes.
+Proof.
+Admitted.
+
+
+Theorem RG_empty_isEmpty : RG_isEmpty RG_empty.
+Proof.
+  compute. reflexivity.
+Qed.
+
+Theorem RG_labNodes_empty_nil : RG_labNodes RG_empty = Empty_set nat.
+Proof.
+Admitted.
+
+
+
+Theorem RG_non_empty_isEmpty_false : forall (nodes : list Node) (edges : list (Node * Node)),
+  length nodes <> 0 <-> not (RG_isEmpty ((RG_mkGraph nodes edges))).
+Proof.
+Admitted.
+
+
+
+
+Theorem RG_matsh_empty_is_nothing : forall (node : Node),
+  RG_matsh node RG_empty = ((False, (Empty_set nat, Empty_set nat)), RG_empty).
+Proof.
+Admitted.
+
+
+
+Theorem RG_spec4 : forall (node : Node) (nodes : list Node) (edges : list (Node * Node)), 
+  List.In node nodes -> exists froms tos, RG_matsh node (RG_mkGraph nodes edges) =
+  ((True, (froms, tos)), RG_mkGraph (filter (fun n => negb (node =? n)) nodes) (filter (fun '(from, to) => negb ((from =? node) || (to =? node))) edges)).
+(* This is not even a complete specification and it looks like a hard one to prove... *)
+Proof.
+Admitted.
+
+
+Theorem RG_spec5 : forall (node : Node) (nodes : list Node) (edges : list (Node * Node)), 
+  not (List.In node nodes) -> RG_matsh node (RG_mkGraph nodes edges) = ((False, (Empty_set nat, Empty_set nat)), RG_mkGraph nodes edges).
+Proof.
+Admitted.
